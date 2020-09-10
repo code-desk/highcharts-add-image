a943381df0519f0c9b03798c37c7008a35e5749c
su- <su-.dev@unitech.jp>
2020-09-10 06:43:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'line',
+    },
+
+    title: {
+        text: 'Setting a plot background image'
+    },
+
+    xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+
+    series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        color: '#303030'
+    }]
+}, function (chartt) { // on complete
+
+    chartt.renderer.image('https://www.highcharts.com/samples/graphics/sun.png',10,10,10,10)
+        .add();
+		
+}
+);
